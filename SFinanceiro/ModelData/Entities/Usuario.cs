f5657e8f5b0d103dcc391cfaa4734ebc5f8d4e9a
Matheus Vieira de Souza <101958548+Matheus-VDS@users.noreply.github.com>
2024-10-30 14:38:39 -0400

    alteração das paginas do CRUD de voluntario

--- SFinanceiro/ModelData/Entities/Usuario.cs
+++ SFinanceiro/ModelData/Entities/Usuario.cs
@@ -1,10 +1,17 @@
-﻿namespace SFinanceiro.ModelData.Entities
+﻿using System.ComponentModel.DataAnnotations;
+namespace SFinanceiro.ModelData.Entities
 {
     public class Usuario
     {
-        public int UsuarioId { get; set; } 
+        public int UsuarioId { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
         public Usuario() { }
