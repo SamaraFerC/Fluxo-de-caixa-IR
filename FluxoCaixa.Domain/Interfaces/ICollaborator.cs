5977fa95bd20349c70bc62a8bf8d0d6ece9038fe
Samara Ferreira <samara.ferreira@infortechms.com.br>
2024-11-04 15:59:21 -0400

    Removida referencia de partial

--- FluxoCaixa.Domain/Interfaces/ICollaborator.cs
+++ FluxoCaixa.Domain/Interfaces/ICollaborator.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluxoCaixa.Domain.Entities;
 
 namespace FluxoCaixa.Domain.Interfaces
 {
     public interface ICollaborator
     {
+        public IEnumerable<Collaborator> GetCollaborators();
     }
 }
