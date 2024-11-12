046a712ca7389354cfb2b515ec4e77db67f22bd5
Matheus Vieira de Souza <101958548+Matheus-VDS@users.noreply.github.com>
2024-11-12 06:15:45 -0400

    menu lateral e alteracao do layout de Atividades

--- FluxoCaixa.Infra.Data/Migrations/20241112064802_PopulaTabela.Designer.cs
+++ FluxoCaixa.Infra.Data/Migrations/20241112064802_PopulaTabela.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FluxoCaixa.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FluxoCaixa.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112064802_PopulaTabela")]
+    partial class PopulaTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
