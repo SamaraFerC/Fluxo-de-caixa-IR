dd4a632c5e3fc135b2a338b1d971bebd61098aaf
Samara Ferreira Candido <43960413+SamaraFerC@users.noreply.github.com>
2024-11-14 11:19:19 -0400

    Ajustado noome de campos

--- FluxoCaixa.Application/ViewModel/PaymentTypeViewModel.cs
+++ FluxoCaixa.Application/ViewModel/PaymentTypeViewModel.cs
@@ -1,10 +1,23 @@
-﻿namespace FluxoCaixa.Application.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluxoCaixa.Application.ViewModel
 {
     public class PaymentTypeViewModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nome")]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Situação")]
         public bool Status { get; set; }
         public string? UserIncluded { get; set; }
         public DateTime? DateIncluded { get; set; }
